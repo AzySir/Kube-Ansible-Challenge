5191e1bb5d9b0a6ea53c02e456c8c94e9c3a86ad
Adam Sir <azy.sir@gmail.com>
2021-01-27 18:13:24 +0100

    Added in variables and auto vars for reusability

---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+
+variable "ami" {
+  type = string
+}
+variable "type" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "az" {
+  type = string
+}
+
+variable "size" {
+  type = number
+}
